3f4d032d59744a7b190d46ff7d63ed2633aaf940
Buruk Aregawi <burukaregawi@Buruks-MacBook-Air.local>
2022-07-10 21:27:07 -0500

    First commit with Spring Boot outline.

---
+++ src/test/java/com/burukyemane/springboot/SpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burukyemane.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
